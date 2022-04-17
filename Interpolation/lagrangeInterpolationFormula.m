ad68e34f63f6ab752be12b5d4161a04ad2654152
Nayeem Fuad <nayeemfuad.bd@gmail.com>
2022-04-17 13:27:06 +0600

    Update lagrangeInterpolationFormula.m

--- Interpolation/lagrangeInterpolationFormula.m
+++ Interpolation/lagrangeInterpolationFormula.m
@@ -1,34 +1,29 @@
-%Solve by Lagrange Interpolation Method
 clear all
 clc
 A = [300, 304, 305, 307];
 B = [2.4771, 2.4829, 2.4843, 2.4871];
 l = length(B);
-x = input("Enter a Number: ");
 y = 0;
-for i = 1 : l
-    num = 1;
-    denum = 1;
-    index = i;
-    tempX = A(i);
-    tempY = B(i);
-    for j = 1 : l
-        if(i ~= j)
-            num = num * (x - A(j));  
-            denum = denum * (tempX - A(j));
-        end      
-    end
-    y = y + tempY * (num / denum);
+x = input("Enter a Point: ");
+for iteration = 1 : l
+   num = 1;
+   denum = 1;
+   for i = 1 : l
+       if(iteration ~= i)
+           num = num * (x - A(i));
+           denum = denum * (A(iteration) - A(i));
+       end
+   end
+   y = y + (num / denum) * B(iteration);
 end
-fprintf("Output: %.4f\n", y);
-plot(A, B);
+fprintf("Interpolated Value Using Lagrange Interpolation at (X = %d): %.4f\n", x, y); 
+plot(A, B, 'red');
 hold on
-plot(x, y, '*')
-xlabel('x-axis')
-ylabel('y-axis')
+plot(x, y, 'black*')
+xlabel('x-axis'); ylabel('y-axis')
 title('Lagrange Interpolation Formula')
-title(legend, 'Pointers')
-legend('Graph', 'Point')
+title(legend, 'Description')
+legend('Given Data', 'New Point')
 text(x + .2, y, 'New Point')
 grid on
 hold off
