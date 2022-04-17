4650509b284c4bb5107d2dcf4fa113dedbc1189b
Nayeem Fuad <nayeemfuad.bd@gmail.com>
2022-04-17 21:11:00 +0600

    Update gaussBackwardFormula.m
    Program Without External Function

--- Interpolation/gaussBackwardFormula.m
+++ Interpolation/gaussBackwardFormula.m
@@ -1,67 +1,71 @@
-% Solve By Gauss Backward Formula
 clear all
 clc
-center = 0;
 A = [1939, 1949, 1959, 1969, 1979, 1989]; 
 B = [12, 15, 20, 27, 39, 52];
-h = A(2) - A(1);
-newMat = differenceTable(A, B);
-[m, n] = size(newMat);
-x = input("Enter a Number: ");
-%Set Center
-for i = 3 : 2 : m
-   if(x <= newMat(i, 1))
-      center = i; 
-      break;
+l = length(B);
+r = 2 * l - 1;
+c = l + 1;
+center = round(r / 2) + 1;
+newMat = zeros(r, c);
+index = 1;
+for i = 1 : 2 : r
+   newMat(i, 1) = A(index);
+   newMat(i, 2) = B(index);
+   index = index + 1;
+end
+for column = 3 : c
+    for row = column - 1 : 2 : r
+        newMat(row, column) = newMat(row + 1, column - 1) - newMat(row - 1, column - 1);
+    end
+    r = r - 1;
+end
+disp("Backward Difference Table")
+disp(newMat)
+x = input("Enter a Point: ");
+h = A(l) - A(l - 1);
+for i = 1 : 2 * l - 1
+   if(x < newMat(i, 1))
+       center = i;
+       break;
    end
 end
-if(x > A(length(A)))
-    center = round(m / 2);
-    if(mod(center, 2) == 0)
-        center = center + 1;
-    end
-end
-p = (x - newMat(center, 1)) / h;
-values = newMat(center, 2 : n);
-temp = newMat(center - 1, 2 : n);
+u = (x - newMat(center, 1)) / h;
+values = newMat(center, 2 : c);
+temp = newMat(center - 1, 2 : c);
 for i = 1 : length(values)
    if(values(i) == 0)
        values(i) = temp(i);
    end
 end
-y = values(1);
-for i = 2 : length(values)
-   y = y + values(i) * (pCalculator(p, i - 1) / factorial(i - 1));
+y = values(1) + u * values(2);
+for i = 3 : l
+    temp = u;
+    n = 1;
+    for j = 3 : i
+        if(mod(j, 2) == 0)
+           temp = temp * (u - n);
+           n = n + 1;
+        else
+            temp = temp * (u + n);
+        end
+    end
+    y = y + (temp / factorial(i-1)) * values(i);
 end
-disp(newMat)
-fprintf("Output: %.4f\n", y);
+fprintf("Interpolation Value using Gauss Backward Interpolation at (X = %.4f): %.4f\n", x, y);
 plot(A, B);
 hold on
 plot(x, y, '*')
-xlabel('x-axis')
-ylabel('y-axis')
+xlabel('x-axis'); ylabel('y-axis')
 title('Gauss Backward Interpolation Formula')
-title(legend, 'Pointers')
-legend('Graph', 'Point')
-text(x + 1, y, 'New Point')
+title(legend, 'Description')
+legend('Given Data', 'New Point')
+text(x + .1, y, 'New Point')
 grid on
 hold off
-function calculatedP = pCalculator(p, t)
-calculatedP = p;
-term = floor(t / 2);
-if t > 1
-    if(mod(t, 2) ~= 0)
-        while term > 0
-            calculatedP = calculatedP * (p - term) * (p + term);
-            term = term - 1;
-        end
-    else
-        temp = term;
-        while term > 0
-            calculatedP = calculatedP * (p -term) * (p + term);
-            term = term - 1; 
-        end
-        calculatedP = calculatedP / (p - temp);
-    end
+function fact = factorial(n)
+fact = 1;
+while(n > 1)
+    fact = fact * n;
+    n = n - 1;
 end
 end
