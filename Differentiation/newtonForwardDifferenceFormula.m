c6e6f40aa57578d5c7df3b89c3b99df873e9d3a5
Nayeem Fuad <nayeemfuad.bd@gmail.com>
2022-04-18 11:57:49 +0600

    Update with User Defined Internal Function

--- Differentiation/newtonForwardDifferenceFormula.m
+++ Differentiation/newtonForwardDifferenceFormula.m
@@ -5,27 +5,34 @@ clc
 syms p
 A = [3, 3.2, 3.4, 3.6, 3.8, 4.0];
 B = [-14.0, -10.032, -5.296, .256, 6.672, 14.0];
-l = length(B) + 1;
-h = A(2) - A(1);
-y = B(1);
-x = input("Enter a Point: ");
-o = input("Enter Order of The Differential Equation: ");
-u = (x - A(1)) / h;
-newMat = differenceTable(A, B);
-for column = 3 : l
-    row = column - 1;
-    y = y + (pCalculator(column - 2) / factorial(column - 2)) * newMat(row, column);
+l = length(B);
+m = l - 1;
+newMat = B';
+for column = 2 : l
+    for row = 1 : m
+        newMat(row, column) = newMat(row + 1, column - 1) - newMat(row, column - 1);
+    end
+    m = m - 1;
 end
-disp(newMat)
-diff_f(p) = (1 / h.^o) * diff(y, p, o);
-fprintf("%d No. Derivative at x(%.2f): %.4f\n", o, x, diff_f(u))
-%P Calculator For Forward Interpolation
-function calculatedP = pCalculator(t)
-syms p
-calculatedP = p;
-term = t;
-while term > 1
-    calculatedP = calculatedP * (p - (term - 1));
-    term = term - 1;
+disp('Forward Difference Table')
+disp([A', newMat])
+y = B(1);
+h = A(2) - A(1);
+x = input("Enter a Point: ");
+u = (x - A(1)) / h;
+term = 1;
+for i = 2 : l
+   term = term * (p + 2 - i);  
+   y = y + (term / factorial(i - 1)) * newMat(1, i);
+end
+f(p) = y;
+o = input("Enter Order: ");
+diff_f(p) = (1 / h.^2) * diff(f(p), p, o);
+fprintf("No. of %d Derivative Using Newton Forward Interpolation at (X = %.4f): %.4f\n", o, x, diff_f(u));
+function fact = factorial(n)
+fact = 1;
+while(n > 1)
+    fact = fact * n;
+    n = n - 1;
 end
 end
