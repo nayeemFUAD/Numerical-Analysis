9e58d7b640cbe064a83aa69f4d438bc6ab9499db
Nayeem Fuad <nayeemfuad.bd@gmail.com>
2022-04-18 11:15:50 +0600

    Update with Internal User Defined Function

--- System Of Linear Equations Solve/gaussSeidelMethod.m
+++ System Of Linear Equations Solve/gaussSeidelMethod.m
@@ -1,56 +1,69 @@
 %Solved By Gauss Seidal Method
 clear all
 clc
-fprintf("\t\t\t\t\t\t\tSolved By Gauss Seidel Iteration Method\n\n");
-tolerance = .0001;
+fprintf("\t\t\t\t\t\t\tSolved By Gauss Seidel Iteration Method\n");
 A = [3, 4, 15; 1, 12, 3; 10, 1, -2];
 B = [54.8; 39.66; 7.74];
-newMat = [A, B];
-disp("Given System Of Linear Equations")
-disp(newMat)
-if(diagonallyDominantTest(A) == 0)
-    disp("Given System of Linear Equations Are not in Diagonally Dominant Form")
-    disp("Converting It into Diagonally Dominant Form")
-    newMat = makeDiagonallyDominant(A, B);
-    [R, C] = size(newMat);
-    A = newMat(:, 1 : C - 1);
-    B = newMat(:, C);
-    disp(newMat)
-end
-if(diagonallyDominantTest(A) == 0)
-    disp("Can't Able to Convert It Into Diagonally Dominant Form")
-    disp("Can't Be Solved. Please Try Again.")
-    return;
-end
-[row, column] = size(A);
-previousX = zeros(1, length(B));
-presentX = previousX;
-c = 0;
-iteration = 0;
-while(c == 0)
-    for i = 1 : row  
-        X = B(i);
-        for j = 1 : column
-            if(i ~= j)
-               X = X - presentX(j) * A(i, j); 
+disp("Given Matrix")
+disp([A, B])
+if(~diagonallyDominantTest(A))
+    disp("Given Matrix Data's Are Not Diagonally Dominant")
+    disp("Trying to Convert it Into Diagonally Dominant Matrix")
+    [r, c] = size(A);
+    for iteration = 1 : r
+        for row = 1 : r - 1
+            sum = 0;
+            for column = 1 : c
+                if(row ~= column)
+                    sum = sum + A(row, column);
+                end
+            end
+            if(A(row, row) < sum)
+                tempA = A(row, :);                      tempB = B(row);
+                A(row, :) = A(row + 1, :);              B(row) = B(row + 1);
+                A(row + 1, :) = tempA;                   B(row + 1) = tempB;
             end
         end
-        X = X / A(i, i);
-        presentX(i) = X;
     end
-    count = 0;
-    for k = 1 : length(presentX)
-       if(abs(previousX(k) - presentX(k)) < tolerance)
-           count = count + 1;
+    disp([A, B])
+    if(~diagonallyDominantTest(A))
+        disp("This Given Matrix Cann't Be Convert Into a Diagonally Dominant Matrix")
+        return;
+    end
+end
+n = input("No. Of Iteration: ");
+values = zeros(1, c);
+for i = 1 : n
+   for row  = 1 : r
+       sum = 0;
+       for column = 1 : c
+           if(row ~= column)
+              sum = sum + A(row, column) * values(column); 
+           end
        end
+       values(row) = (B(row) - sum) / A(row, row);
+   end
+end
+fprintf("After No. of %d Iteration We get\n", n);
+for i = 1 : length(values)
+    fprintf("X(%d) = %.4f\n", i, values(i));
+end
+function d = digonallyDominantTest(A)
+[r, c] = size(A);
+count = 0;
+d = 0;
+for row = 1 : r
+    sum = 0;
+    for column = 1 : c
+        if(row ~= column)
+            sum = sum + A(row, column);
+        end
     end
-    if(count == length(presentX))
-       fprintf("Solution Of the Given System of Equations Found Between %d and %d No. Iteration\n", iteration - 1, iteration);
-       for index = 1 : length(presentX)
-          fprintf("x(%d): %.4f\n", index, presentX(index)); 
-       end
-       break;
+    if(A(row, row) >= sum)
+        count = count + 1;
     end
-    previousX = presentX;
-    iteration = iteration + 1;
+end
+if(count == 3)
+    d = 1;
+end
 end
