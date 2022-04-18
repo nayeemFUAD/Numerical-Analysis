21e007dea16aabc63205cc7c557e4de4260b3b7b
Nayeem Fuad <nayeemfuad.bd@gmail.com>
2022-04-18 12:18:13 +0600

    Update lagrangeDifferentiationFormula.m

--- Differentiation/lagrangeDifferentiationFormula.m
+++ Differentiation/lagrangeDifferentiationFormula.m
@@ -1,26 +1,23 @@
-%Solve by Lagrange Differentiation Formula
 clear all
 clc
-syms x
+syms p
 A = [2, 4, 9, 10];
 B = [4, 56, 711, 980];
 l = length(B);
-u = input("Enter a Number: ");
-o = input("Enter Order of The Differential Equation: ");
 y = 0;
-for i = 1 : l
-    num = 1;
-    denum = 1;
-    index = i;
-    tempX = A(i);
-    tempY = B(i);
-    for j = 1 : l
-        if(i ~= j)
-            num = num * (x - A(j));  
-            denum = denum * (tempX - A(j));
-        end      
-    end
-    y = y + tempY * (num / denum);
+x = input("Enter a Point: ");
+for iteration = 1 : l
+   num = 1;
+   denum = 1;
+   for i = 1 : l
+       if(iteration ~= i)
+           num = num * (p - A(i));
+           denum = denum * (A(iteration) - A(i));
+       end
+   end
+   y = y + (num / denum) * B(iteration);
 end
-diff_f(x) = diff(y, x, o);
-fprintf("%d No. Derivative at x(%.2f): %.4f\n", o, u, diff_f(u))
+f(p) = y;
+o = input("Enter Order: ");
+diff_f(p) = diff(f(p), p, o);
+fprintf("No. of %d Differentiation Using Lagrange Interpolation at (X = %.4f): %.4f\n", o, x, diff_f(x)); 
