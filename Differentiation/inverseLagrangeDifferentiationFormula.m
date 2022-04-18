ba95b15838d995f5f656863c6ea74eeea6d8e943
Nayeem Fuad <nayeemfuad.bd@gmail.com>
2022-04-18 12:23:19 +0600

    Update inverseLagrangeDifferentiationFormula.m

--- Differentiation/inverseLagrangeDifferentiationFormula.m
+++ Differentiation/inverseLagrangeDifferentiationFormula.m
@@ -1,26 +1,23 @@
-%Solve by Lagrange Inverse Differentiation Formula
 clear all
 clc
-syms y
+syms p
 A = [2, 4, 9, 10];
 B = [4, 56, 711, 980];
 l = length(B);
-u = input("Enter a Number: ");
-o = input("Enter Order of The Differential Equation: ");
 x = 0;
-for i = 1 : l
-    num = 1;
-    denum = 1;
-    index = i;
-    tempX = A(i);
-    tempY = B(i);
-    for j = 1 : l
-        if(i ~= j)
-            num = num * (y - B(j));  
-            denum = denum * (tempY - B(j));
-        end      
-    end
-    x = x + tempX * (num / denum);
+y = input("Enter a Point: ");
+for iteration = 1 : l
+   num = 1;
+   denum = 1;
+   for i = 1 : l
+       if(iteration ~= i)
+           num = num * (p - B(i));
+           denum = denum * (B(iteration) - B(i));
+       end
+   end
+   x = x + (num / denum) * A(iteration);
 end
-diff_f(y) = diff(x, y, o);
-fprintf("%d No. Derivative at y(%.2f): %.4f\n", o, u, diff_f(u))
+f(p) = x;
+o = input("Enter Order: ");
+diff_f(p) = diff(f(p), p, o);
+fprintf("No. of %d Differentiation Using Lagrange Inverse Interpolation at (Y = %.4f): %.4f\n", o, y, diff_f(y)); 
