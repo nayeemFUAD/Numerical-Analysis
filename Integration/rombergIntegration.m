a827e569d41de9cc1d03ff9fd604ccd19c06300f
Nayeem Fuad <nayeemfuad.bd@gmail.com>
2022-04-18 21:01:51 +0600

    Update rombergIntegration.m

--- Integration/rombergIntegration.m
+++ Integration/rombergIntegration.m
@@ -1,50 +1,50 @@
 %Solved By Romberg Integration Formula
 clear all
 clc
+disp("integration Solved By Romberg Integration")
 f = @(x) (x.^2);
-a = input("Enter Lower Limit: ");          %Lower Limit
-b = input("Enter Upper Limit: ");          %Upper Limit
-h = (b - a) / 2;        
-i = 1;
-while(h >= .5)
-   I(i) = (h / 2) * (f(a) + 2 * calculator(f, a, b, h) + f(b));
-   H(i) = h;
-   i = i + 1;
-   h = h / 2; 
-end
-rows = 2 * length(H) - 1;
-columns = length(H) + 1;
-newMat = zeros(rows, columns);
+a = input("Enter Lower Limit: ");
+b = input("Enter Upper Limit: ");
+h = (b - a) / 2;
 index = 1;
-for i = 1 : 2 : rows
-    newMat(i, 1) = H(index);
-    newMat(i, 2) = I(index);
+while(h >= .5)
+    newMat(index, 1) = h;
+    newMat(index, 2) = trapezoidalRule(h, a, b, f); 
     index = index + 1;
+    h = h / 2;
 end
-for j = 3 : 1 : columns
-    for i = j - 1 : 2 : rows
-        newMat(i, j) = newMat(i + 1, j - 1) + (newMat(i + 1, j - 1) - newMat(i - 1, j - 1)) / 3;
-    end
-    rows = rows - 1;
+[r, c] = size(newMat);
+m = r - 1;
+for column = 3 : r + 1
+   for row = 1 : m
+       newMat(row, column) = newMat(row+1, column-1) + (newMat(row+1, column-1)-newMat(row, column-1))/3;
+   end
+   m = m - 1;
 end
 disp(newMat)
-fprintf("Integration Value of the given function: %.4f\n", newMat(rows, columns));
-X = a : .1 : b;
-n = ((b - a)) / .1 + 1;
-for i = 1 : n
+fprintf("Integration Value of the given function: %.4f\n", newMat(1, r + 1));
+X = a : .01 : b;
+for i = 1 : length(X)
    Y(i) = f(X(i)); 
 end
 area(X, Y)
 hold on
-title(legend, 'Pointers')
+title(legend, 'Description')
 legend('Area Of The Given f(x)')
 grid on
 title("Integration Using Romberg Integration")
 xlabel('X-Axis'); ylabel('Y-Axis');
 hold off
-function val = calculator(f, a, b, h)
-val = 0;
-for i = a : (b / h) - 1
-    val = val + f(a + i * h);
+function t = trapezoidalRule(h, a, b, f)
+x = a : h : b;
+sum = 0;
+for i = 1 : length(x)
+   y(i) = f(x(i));
+   if(i == 1 || i == length(x))
+     sum = sum + y(i);
+   else
+       sum = sum + 2 * y(i);
+   end
 end
+t = (h / 2) * sum;
 end
