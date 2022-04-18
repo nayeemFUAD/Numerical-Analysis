93539ee866271a3eca451541ad899492fb90f161
Nayeem Fuad <nayeemfuad.bd@gmail.com>
2022-04-18 12:04:52 +0600

    Update Using Internal User Defined Function

--- Differentiation/newtonBackwardDifferentiationFormula.m
+++ Differentiation/newtonBackwardDifferentiationFormula.m
@@ -1,32 +1,34 @@
-% Solved By Newton Backward Difference Formula
-%Titas P-237 EX-23
 clear all
 clc
 syms p
 A = [1, 1.05, 1.10, 1.15, 1.20, 1.25, 1.30];
 B = [1, 1.0247, 1.0488, 1.0724, 1.0955, 1.1180, 1.1402];
-l = length(B) + 1;
-h = A(2) - A(1);
-y = B(length(B));
-x = input("Enter a Point: ");
-o = input("Enter Order of The Differential Equation: ");
-u = (x - A(length(A))) / h;
-newMat = differenceTable(A, B);
-row = 2 * length(B) - 1;
-for column = 3 : l
-    row = row - 1;
-    y = y + (pCalculator(column - 2) / factorial(column - 2)) * newMat(row, column);
+l = length(B);
+newMat = B';
+for column = 2 : l
+    for row = l : -1 : column
+        newMat(row, column) = newMat(row, column - 1) - newMat(row - 1, column -1);
+    end
 end
-disp(newMat)
-diff_f(p) = (1 / h.^o) * diff(y, p, o);
-fprintf("%d No. Derivative at x(%.2f): %.4f\n", o, x, diff_f(u))
-%P Calculator For Backward Interpolation
-function calculatedP = pCalculator(t)
-syms p
-calculatedP = p;
-term = t;
-while term > 1
-    calculatedP = calculatedP * (p + (term - 1));
-    term = term - 1;
+disp('Backward Difference Table')
+disp([A', newMat])
+y = B(l);
+h = A(l) - A(l - 1);
+x = input("Enter a Point: ");
+u = (x - A(l)) / h;
+term = 1;
+for i = 2 : l
+   term = term * (p - 2 + i);  
+   y = y + (term / factorial(i - 1)) * newMat(l, i);
+end
+f(p) = y;
+o = input("Enter Order: ");
+diff_f(p) = (1 / h.^o) * diff(f(p), p, o);
+fprintf("No. of %d Differentiation Using Newton Backward Interpolation at (X = %.4f): %.4f\n", o, x, diff_f(u));
+function fact = factorial(n)
+fact = 1;
+while(n > 1)
+    fact = fact * n;
+    n = n - 1;
 end
 end
