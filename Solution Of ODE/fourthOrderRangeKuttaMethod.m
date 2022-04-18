32d12fcc4d3617b5a5c04e9c505d29ceec5038ec
Nayeem Fuad <nayeemfuad.bd@gmail.com>
2022-04-18 23:04:03 +0600

    Update fourthOrderRangeKuttaMethod.m

--- Solution Of ODE/fourthOrderRangeKuttaMethod.m
+++ Solution Of ODE/fourthOrderRangeKuttaMethod.m
@@ -1,21 +1,24 @@
-%Fourth Order Range-Kutta Method For ODE
 clear all
 clc
-f = @(x, y) (-2 * x - y);
+f = @(x, y) ((x.^2 + y.^2) / 10);
 x(1) = 0;
-y(1) = -1;
-h = .1;
-u = input("Enter a Point to Find it's Solution: ");
-n = round(u / h);
-for i = 2 : n + 1
-   x(i) = x(i - 1) + h; 
-end
+y(1) = 1;
+u = input("Enter a Point to Find It's Solution: ");
+h = input("Enter Increament: ");
+x = x(1) : h : u;
 for i = 2 : length(x)
    k1 = h * f(x(i - 1), y(i - 1));
    k2 = h * f(x(i - 1) + (h / 2), y(i - 1) + (k1 / 2));
    k3 = h * f(x(i - 1) + (h / 2), y(i - 1) + (k2 / 2));
    k4 = h * f(x(i - 1) + h, y(i - 1) + k3);
-   y(i) = y(i - 1) + (k1 + 2 * k2 + 2 * k3 + k4) / 6;
+   y(i) = y(i - 1) + (1 / 6) * (k1 + 2 * k2 + 2 * k3 + k4);
 end
-fprintf("The Solution Of The Given ODE at Point(x = %.2f): %.4f\n", u, y(length(y)));
+fprintf("Solution Of ODE Using RK4 Method at X(%.2f): %.4f\n", u, y(length(y)));
 plot(x, y)
+hold on
+title("Solution Of ODE Using RK4 Method")
+title(legend, 'Description')
+legend('RK4')
+grid on
+xlabel('X-Axis'); ylabel('Y-Axis')
+hold off
