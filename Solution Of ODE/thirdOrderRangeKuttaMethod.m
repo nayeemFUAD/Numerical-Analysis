1004b6e5a7e2749bb7e934aa89639f36def8fa44
Nayeem Fuad <nayeemfuad.bd@gmail.com>
2022-04-18 23:01:30 +0600

    Update thirdOrderRangeKuttaMethod.m

--- Solution Of ODE/thirdOrderRangeKuttaMethod.m
+++ Solution Of ODE/thirdOrderRangeKuttaMethod.m
@@ -1,20 +1,23 @@
-%Third Order Range-Kutta Method For ODE
 clear all
 clc
-f = @(x, y) (-2 * x - y);
+f = @(x, y) ((x.^2 + y.^2) / 10);
 x(1) = 0;
-y(1) = -1;
-h = .1;
-u = input("Enter a Point to Find it's Solution: ");
-n = round(u / h);
-for i = 2 : n + 1
-   x(i) = x(i - 1) + h; 
-end
+y(1) = 1;
+u = input("Enter a Point to Find It's Solution: ");
+h = input("Enter Increament: ");
+x = x(1) : h : u;
 for i = 2 : length(x)
    k1 = h * f(x(i - 1), y(i - 1));
    k2 = h * f(x(i - 1) + (h / 2), y(i - 1) + (k1 / 2));
    k3 = h * f(x(i - 1) + h, y(i - 1) + 2 * k2 - k1);
-   y(i) = y(i - 1) + (k1 + 4 * k2 + k3) / 6;
+   y(i) = y(i - 1) + (1 / 6) * (k1 + 4 * k2 + k3);
 end
-fprintf("The Solution Of The Given ODE at Point(x = %.2f): %.4f\n", u, y(length(y)));
+fprintf("Solution Of ODE Using RK3 Method at X(%.2f): %.4f\n", u, y(length(y)));
 plot(x, y)
+hold on
+title("Solution Of ODE Using RK3 Method")
+title(legend, 'Description')
+legend('RK3')
+grid on
+xlabel('X-Axis'); ylabel('Y-Axis')
+hold off
