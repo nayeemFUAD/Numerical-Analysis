3ad48def240c346e4d520753ee3973930c3735b5
Nayeem Fuad <nayeemfuad.bd@gmail.com>
2022-04-18 22:58:49 +0600

    Update secondOrderRangeKuttaMethod.m

--- Solution Of ODE/secondOrderRangeKuttaMethod.m
+++ Solution Of ODE/secondOrderRangeKuttaMethod.m
@@ -1,20 +1,22 @@
-%Second Order Range-Kutta Method For ODE
 clear all
 clc
-f = @(x, y) (-2 * x - y);
+f = @(x, y) ((x.^2 + y.^2) / 10);
 x(1) = 0;
-y(1) = -1;
-h = .1;
-u = input("Enter a Point to Find it's Solution: ");
-n = round(u / h);
-for i = 2 : n + 1
-   x(i) = x(i - 1) + h; 
-end
-
+y(1) = 1;
+u = input("Enter a Point to Find it's solution: ");
+h = input("Enter Increament: ");
+x = x(1) : h : u;
 for i = 2 : length(x)
    k1 = h * f(x(i - 1), y(i - 1));
-   k2 = h * f(x(i - 1) + (h / 2), y(i - 1) + (k1 / 2));
-   y(i) = y(i - 1) + k2;
+   k2 = h * f(x(i - 1) + h, y(i - 1) + k1);
+   y(i) = y(i - 1) + ((k1 + k2) / 2);
 end
-fprintf("The Solution Of The Given ODE at Point(x = %.2f): %.4f\n", u, y(length(y)));
+fprintf("Solution Of ODE Using RK2 Method at X(%.2f): %.4f\n", u, y(length(y)));
 plot(x, y)
+hold on
+title("Solution Of ODE Using RK2 Method")
+title(legend, 'Description')
+legend('RK2')
+grid on
+xlabel('X-Axis'); ylabel('Y-Axis')
+hold off
