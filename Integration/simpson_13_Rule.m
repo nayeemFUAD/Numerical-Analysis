7760e4eca13d73ef33f7e1837c853bc7eb204c23
Nayeem Fuad <nayeemfuad.bd@gmail.com>
2022-04-18 19:38:44 +0600

    Update simpson_13_Rule.m

--- Integration/simpson_13_Rule.m
+++ Integration/simpson_13_Rule.m
@@ -1,41 +1,35 @@
-%Simpson's 1/3 Rule for Numerical Integration
-%TITAS P-261 EX-3
 clear all
 clc
 disp("Numerical Integration Using Simpson's 1/3 Rule")
 f = @(x) (sqrt(sin(x) + cos(x)));
-%Set Definite Range
-a = input("Enter Lower Range: ");
-b = input("Enter Upper Range: ");
-n = input("Enter Total Sub Divisions: ");
+a = input("Enter Lower Limit: ");
+b = input("Enter Upper Limit: ");
+n = input("Enter Number Of Sub-Divisions: ");
 h = (b - a) / n;
-y = f(a) + f(b);
-sumEven = 0;
-sumOdd = 0;
-for i = 1 : n - 1
-% table for getting functional value
-   newMat(i, 1) = a + i * h;
-   newMat(i, 2) = f(newMat(i, 1));
-   if(mod(i, 2) == 0)
-       sumEven = sumEven + newMat(i, 2);
+x = a : h : b;
+sum = 0;
+for i = 1 : length(x)
+   y(i) = f(x(i));
+   if(i == 1 || i == length(x))
+       sum = sum + y(i);
    else
-       sumOdd = sumOdd + newMat(i, 2);
+       if(mod(i, 2) == 0)
+           sum = sum + 4 * y(i);
+       else
+           sum = sum + 2 * y(i);
+       end
    end
 end
-y = (h / 3) * (y + (4 * sumOdd + 2 * sumEven));
-fprintf("Integration Value of the given function: %.4f\n", y);
-X = a : h : b;
-for i = 1 : n + 1
-   Y(i) = f(X(i)); 
-end
-area(X, Y)
+answer = (h / 3) * sum;
+fprintf("Integration Value of the given function: %.4f\n", answer);
+area(x, y)
 hold on
-bar(X, Y, .01, 'black')
-title(legend, 'Pointers')
-legend('Divided Area')
-grid on
+bar(x, y, .01, 'black')
 axis on
-title("Integration Using Simpson's 1/3 Rule")
-xlabel('X-Axis'); ylabel('Y-Axis');
-axis([a - .3 b + .3 a - 1 2])
+axis([a b -answer answer])
+grid on
+title("Solved By Simpson's 1/3 Rule")
+title(legend, "Description")
+legend("Given Function", "Slides")
+xlabel("X-Axis"); ylabel("Y-Axis")
 hold off
