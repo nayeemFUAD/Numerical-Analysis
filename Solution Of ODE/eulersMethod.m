dafa72ca63f3fd83137c44769fde1b20b41d65df
Nayeem Fuad <nayeemfuad.bd@gmail.com>
2022-04-18 21:16:47 +0600

    Update eulersMethod.m

--- Solution Of ODE/eulersMethod.m
+++ Solution Of ODE/eulersMethod.m
@@ -1,19 +1,15 @@
-%Euler's Method to Solve ODE
 clear all
 clc
-f = @(x, y) ((y-x) / (y+x));
+f = @(x, y) ((y - x) / (y + x));
 x(1) = 0;
 y(1) = 1;
-h = 0.02;
-u = input("Enter a Point to Find it's Solution: ");
-n = round(u / h) + 1;
-for i = 2 : n
-    x(i) = x(i - 1) + h;
-end
+u = input("Enter a Point To Find It's Solution: ");
+h = input("Enter Increament: ");
+x = x(1) : h : u;
 for i = 2 : length(x)
     y(i) = y(i - 1) + h * f(x(i - 1), y(i - 1));
 end
-fprintf("The Solution Of The Given ODE at Point(x = %.2f): %.4f\n", u, y(length(y)));
+fprintf("The Solution Of The Given ODE at Point (X = %.2f): %.4f\n", u, y(length(y)));
 plot(x, y)
 hold on
 title("Solution Of ODE Using Euler's Method")
