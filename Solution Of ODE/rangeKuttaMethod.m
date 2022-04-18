85c8aebb3405694ec2a5ed42a24e05b658966174
Nayeem Fuad <nayeemfuad.bd@gmail.com>
2022-04-18 22:45:53 +0600

    Update rangeKuttaMethod.m

--- Solution Of ODE/rangeKuttaMethod.m
+++ Solution Of ODE/rangeKuttaMethod.m
@@ -1,39 +1,36 @@
 clear all
 clc
-f = @(x, y) (-2 * x - y);
+f = @(x, y) ((x.^2 + y.^2) / 10);
 x(1) = 0;
-y(1) = -1;
+y(1) = 1;
+u = input("Enter a Point to Find It's Solution: ");
 h = input("Enter Increament: ");
-u = input("Enter a Point To Find It's Solution: ");
-n = round(u / h) + 1;
-for i = 2 : n
-   x(i) = x(i - 1) + h; 
-end
+x = x(1) : h : u;
 for i = 2 : length(x)
    k1 = h * f(x(i - 1), y(i - 1));
-   k2 = h * f(x(i - 1) + (h / 2), y(i - 1) + (k1 / 2));
-   y(i) = y(i - 1) + k2;
+   k2 = h * f(x(i - 1) + h, y(i - 1) + k1);
+   y(i) = y(i - 1) + (k1 + k2) / 2;
 end
-fprintf("The Solution Of The Given ODE using RK2 at Point(x = %.2f): %.4f\n", u, y(length(y)));
+fprintf("Solution Of ODE Using RK2 Method at X(%.2f): %.4f\n", u, y(length(y)));
 plot(x, y, 'red')
 hold on
 for i = 2 : length(x)
    k1 = h * f(x(i - 1), y(i - 1));
    k2 = h * f(x(i - 1) + (h / 2), y(i - 1) + (k1 / 2));
    k3 = h * f(x(i - 1) + h, y(i - 1) + 2 * k2 - k1);
-   y(i) = y(i - 1) + (k1 + 4 * k2 + k3) / 6;
+   y(i) = y(i - 1) + (1 / 6) * (k1 + 4 * k2 + k3);
 end
-fprintf("The Solution Of The Given ODE using RK3 at Point(x = %.2f): %.4f\n", u, y(length(y)));
-plot(x, y, 'blue')
+fprintf("Solution Of ODE Using RK3 Method at X(%.2f): %.4f\n", u, y(length(y)));
+plot(x, y, 'green')
 for i = 2 : length(x)
    k1 = h * f(x(i - 1), y(i - 1));
    k2 = h * f(x(i - 1) + (h / 2), y(i - 1) + (k1 / 2));
    k3 = h * f(x(i - 1) + (h / 2), y(i - 1) + (k2 / 2));
    k4 = h * f(x(i - 1) + h, y(i - 1) + k3);
-   y(i) = y(i - 1) + (k1 + 2 * k2 + 2 * k3 + k4) / 6;
+   y(i) = y(i - 1) + (1 / 6) * (k1 + 2 * k2 + 2 * k3 + k4);
 end
-fprintf("The Solution Of The Given ODE using RK4 at Point(x = %.2f): %.4f\n", u, y(length(y)));
-plot(x, y, 'green')
+fprintf("Solution Of ODE Using RK4 Method at X(%.2f): %.4f\n", u, y(length(y)));
+plot(x, y, 'blue')
 title("Solution Of ODE Using Range-Kutta Method")
 title(legend, 'Graph')
 legend('RK2', 'RK3', 'RK4')
