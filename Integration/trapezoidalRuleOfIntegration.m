3ff9377d5befac6b8c2d77035acb088ec6bc0609
Nayeem Fuad <nayeemfuad.bd@gmail.com>
2022-04-18 13:36:18 +0600

    Update trapezoidalRuleOfIntegration.m

--- Integration/trapezoidalRuleOfIntegration.m
+++ Integration/trapezoidalRuleOfIntegration.m
@@ -4,33 +4,30 @@ clear all
 clc
 disp("Numerical Integration Using Trapezoidal Rule")
 f = @(x) (1 / (1 + x.^2));
-%Set Definite Range
 a = input("Enter Lower Range: ");
 b = input("Enter Upper Range: ");
-n = input("Enter Total Number of Sub Divisions: ");
+n = input("Enter total number of Sub-Divisions: ");
 h = (b - a) / n;
-y = .5 * (f(a) + f(b));
+x = a : h : b;
 sum = 0;
-for i = 1 : n - 1
-% table for getting functional value
-   newMat(i, 1) = a + i * h;
-   newMat(i, 2) = f(newMat(i, 1));
-   sum = sum + newMat(i, 2);
+for i = 1 : length(x)
+    y(i) = f(x(i));
+    if(i == 1 || i == length(x))
+        sum = sum + y(i);
+    else
+        sum = sum + 2 * y(i);
+    end
 end
-y = h * (y + sum);
-fprintf("Integration Value of the given function: %.4f\n", y);
-X = a : h : b;
-for i = 1 : n + 1
-   Y(i) = f(X(i)); 
-end
-area(X, Y)
+answer = (h / 2) * sum;
+fprintf("Integration Value of the given function: %.4f\n", answer);
+area(x, y)
 hold on
-bar(X, Y, .01, 'black')
-title(legend, 'Pointers')
-legend('Area Bounded By Each Trapezium')
+bar(x, y, .01, 'black')
+title(legend, 'Description')
+legend('Area Bounded By Each Trapezium', 'Slides')
 grid on
 axis on
 title("Integration Using Trapezoidal Rule")
 xlabel('X-Axis'); ylabel('Y-Axis');
-axis([a - .3 b + .3 a - 1 b + 1])
+axis([a b -answer answer])
 hold off
