f8c6b6da0ae468faa0675b729cecdc9b149031bf
Nayeem Fuad <nayeemfuad.bd@gmail.com>
2022-04-19 13:03:58 +0600

    Update newtonRaphsonMethod.m

--- Root Findings/newtonRaphsonMethod.m
+++ Root Findings/newtonRaphsonMethod.m
@@ -1,46 +1,42 @@
-%Find Root of an Equation Using Newton Raphson Method
 clear all
 clc
 f = @(x) (x.^3 - x - 1);
 diff_f = @(x) (3 * x.^2 - 1);
-tollerance = 0.001;
-lRange = input("Enter Lower Range Of The Given Continuous Function: ");
-uRange = input("Enter Upper Range Of The Given Continuous Function: ");
-if(f(lRange) * f(uRange) < 0)
-    fprintf("At Least One Root Lies Between The Given Range [%d, %d]\n", lRange, uRange);
+tolerance = .001;
+x(1) = input("Enter Your First Assumption: ");
+if(abs(f(x(1))) < tolerance)
+    disp("Your Assumption is Almost Exact Root Of The Equation");
+    fprintf("Iteration 1: At (X = %.4f): f(%.4f) = %.4f\n", x(1), x(1), f(x(1)));
+    fprintf("Root: %.4f\n", x(1));
+    plot(x(1), f(x(1)), 'black*')
+    hold on
+    text(x(1), f(x(1)), '  Root')
 else
-    fprintf("No Root Lies Between The Given Range [%d, %d]\m", lRange, uRange);
-    return;
-end
-iGuess = (lRange + uRange) / 2;
-if(f(iGuess) == 0)
-    printf("Root: %.4f\n", iGuess);
-else
-    i = 1;
-    fprintf("Iteration %d: At(x = %.4f): f(%.4f) = %.4f\n", i, iGuess, iGuess, f(iGuess));
-    count = 0; i = 2; temp = f(iGuess);
-    while(count == 0)
-       x = iGuess - (f(iGuess) / diff_f(iGuess));
-       fprintf("Iteration %d: At(x = %.4f): f(%.4f) = %.4f\n", i, x, x, f(x));
-       if(abs(round(f(x), 3)) <= tollerance)
-           fprintf("\nRoot: %.4f\nFound at %d No. Iteration\n", x, i);
-           break;
-       else
-           temp = f(x);
-       end
-       iGuess = x;
-       i = i + 1;
+    fprintf("Iteration 1: At (X = %.4f): f(%.4f) = %.4f\n", x(1), x(1), f(x(1)));
+    i = 2;
+while(1)
+    x(i) = x(i - 1) - (f(x(i - 1))/ diff_f(x(i - 1)));
+    fprintf("Iteration %d: At (X = %.4f): f(%.4f) = %.4f\n", i, x(i), x(i), f(x(i)));
+    if(abs(f(x(i))) < tolerance)
+        fprintf("Root: %.4f\nFound at %d No. Of Assumption.\n", x(i), i);
+        plot(x(i), f(x(i)), 'black*')
+        hold on
+        text(x(i), f(x(i)), '  Root')
+        break;
     end
+    i = i + 1;
 end
-u = lRange - 5 : .01 : uRange + 5;
-plot(u, f(u), 'red')
-hold on
+end
+X = -2 : .01 : 2;
+for i = 1 : length(X)
+    Y(i) = f(X(i));
+end
+plot(X, Y, 'red')
+xline(0, 'linewidth', 1.0, 'color', 'green')
+yline(0, 'linewidth', 1.0, 'color', 'green')
+title("Solved By Newton Raphson Method")
+title(legend, "Description")
+legend("Root", "Graph Of The Given Function", "X-Line", "Y-Line")
+xlabel("X-Axis"); ylabel("Y-Axis")
 grid on
-plot(iGuess, f(iGuess), 'black*')
-text(iGuess + 1, f(iGuess) + 1, "  Intersecting Point")
-xlabel('X-Axis'); ylabel('Y-Axis');
-title("Finding Root Using Newton Raphson Method")
-title(legend, 'Pointer')
-xline(0, 'linewidth', 1.0, 'color', 'green'); yline(0, 'linewidth', 1.0, 'color', 'green');
-legend('Graph Of a Function', 'Intersecting Point')
 hold off
